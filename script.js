34fa2c6a89604a316621afacb9ed0f3147928448
AntSevestre <AntSevestre@outlook.fr>
2023-04-21 13:33:27 +0200

    Correction

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let darkMode = false;
-let secretMode = false;
-let clickCount = 0;
 
 function changeDarkMode(){
     if(darkMode){
@@ -8,38 +6,12 @@ function changeDarkMode(){
         darkMode = false;
         document.documentElement.style.setProperty("--text-color", "#202124");
         document.documentElement.style.setProperty("--background-color", "#efe7e5");
-        document.getElementById("dark_light_mode").innerHTML = "Dark mode";
+        document.getElementById("dark_light_mode").innerHTML = "Dark mode";  
     }else{
         // dark mode
         darkMode = true;
         document.documentElement.style.setProperty("--text-color", "white");
         document.documentElement.style.setProperty("--background-color", "#202124");
         document.getElementById("dark_light_mode").innerHTML = "Light mode";
-    }
-}
-
-function bonus(){
-    clickCount += 1;
-    console.log(clickCount);
-
-    if(clickCount == 3){
-
-        if(secretMode){
-            console.log("See ya !");
-
-            secretMode = false;
-            document.documentElement.style.setProperty("--text-color", "#202124");
-            document.documentElement.style.setProperty("--background-color", "#efe7e5");
-            document.getElementById("dark_light_mode").innerHTML = "Light mode";
-        }else{
-            console.log("Well done you discovered the funky mode");
-
-            secretMode = true;
-            document.documentElement.style.setProperty("--text-color", "orange");
-            document.documentElement.style.setProperty("--background-color", "#202124");
-            document.getElementById("dark_light_mode").innerHTML = "Secret mode";
-        }
-
-        clickCount = 0;
     }
 }
